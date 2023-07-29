f6020b95b1eda698e56d4681098c3d2c5eaf5125
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-07-29 15:05:34 -0300

    fix: NewTask input on focus border

--- src/components/NewTask/index.tsx
+++ src/components/NewTask/index.tsx
@@ -1,13 +1,21 @@
 import { Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { styles } from './styles'
+import { useState } from 'react'
 
 export function NewTask() {
+  const [isFocused, setIsFocused] = useState(false)
+
   return (
     <View style={styles.container}>
       <TextInput
-        style={styles.input}
+        style={[
+          styles.input,
+          isFocused && { ...styles.input, borderColor: '#5E60CE' },
+        ]}
         placeholder="Adicione uma nova tarefa"
         placeholderTextColor={'#808080'}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
       />
       <TouchableOpacity style={styles.createButton}>
         <Text style={styles.createButtonText}>+</Text>
